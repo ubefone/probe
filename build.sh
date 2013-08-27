986151ff637a3623bbfaa2172ccab27990b57016
Julien Ammous <schmurfy@gmail.com>
2013-08-27 15:01:31 +0200

    improved build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+set -e
+
 export MRUBY_CONFIG=../build_config.rb
-cd mruby && mkdir -p test && touch test/mrbtest.rake && rake
+cd mruby
+mkdir -p test
+touch test/mrbtest.rake
+rake
+cp bin/d3probe ..

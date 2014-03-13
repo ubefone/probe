5decab1a42364e3c4152d4d9fd18381b90d0f959
Julien Ammous <schmurfy@gmail.com>
2014-03-13 14:30:48 +0100

    pf: recursively search for labels

--- main/tools/d3probe/pf.c
+++ main/tools/d3probe/pf.c
@@ -26,22 +26,23 @@ error:
   return mrb_nil_value();
 }
 
-static mrb_value _pf_stats(mrb_state *mrb, mrb_value self)
+
+static int _pf_stats_recursive(mrb_state *mrb, const char *anchor, mrb_value klass, int ai, mrb_value r_ret)
 {
-  int ai, rules_count;
+  int rules_count;
   mrb_int i;
-  mrb_value r_ret;
   struct pfioc_rule pr;
-  struct RClass *c = mrb_class_get(mrb, "PacketFilterLabelStats");
   
   bzero(&pr, sizeof(pr));
+  
+  if( anchor ){
+    strlcpy(pr.anchor, anchor, MAXPATHLEN);
+  }
+  
   if( ioctl(dev, DIOCGETRULES, &pr) )
     ERROUT("Unable to list rules");
   
   rules_count = pr.nr;
-  r_ret = mrb_ary_new(mrb);
-  
-  ai = mrb_gc_arena_save(mrb);
   
   for( i = 0; i< rules_count; i++){
     mrb_value r_tmp;
@@ -50,7 +51,7 @@ static mrb_value _pf_stats(mrb_state *mrb, mrb_value self)
       ERROUTF("Unable to query rule %d", i);
     
     if( pr.rule.label[0] ){
-      r_tmp = mrb_funcall(mrb, mrb_obj_value(c), "new", 6,
+      r_tmp = mrb_funcall(mrb, klass, "new", 6,
           mrb_str_new_cstr(mrb, pr.rule.label),
           mrb_fixnum_value(pr.rule.evaluations),
           mrb_fixnum_value(pr.rule.packets[0]),
@@ -62,12 +63,29 @@ static mrb_value _pf_stats(mrb_state *mrb, mrb_value self)
       mrb_ary_push(mrb, r_ret, r_tmp);
       mrb_gc_arena_restore(mrb, ai);
     }
+    else if( pr.anchor_call[0] ) {
+      _pf_stats_recursive(mrb, pr.anchor_call, klass, ai, r_ret);
+    }
   }
   
-  return r_ret;
+  return 0;
   
 error:
-  return mrb_nil_value();
+  return -1;
+}
+
+
+static mrb_value _pf_stats(mrb_state *mrb, mrb_value self)
+{
+  int ai;
+  mrb_value r_ret;
+  struct RClass *c = mrb_class_get(mrb, "PacketFilterLabelStats");
+  
+  r_ret = mrb_ary_new(mrb);
+  
+  ai = mrb_gc_arena_save(mrb);
+  _pf_stats_recursive(mrb, NULL, mrb_obj_value(c), ai, r_ret);
+  return r_ret;
 }
 
 void setup_pf_api(mrb_state *mrb)

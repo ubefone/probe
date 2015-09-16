42c10dc6c2a761c565bc961bfef38861b4754700
Julien Ammous <schmurfy@gmail.com>
2015-09-16 20:59:42 +0200

    ...

--- plugins_c/src/statsd.c
+++ plugins_c/src/statsd.c
@@ -133,7 +133,7 @@ static mrb_value _network_loop(mrb_state *mrb, mrb_value self)
     FD_SET(socket, &rd_set);
     
     if( select(maxfd, &rd_set, NULL, NULL, NULL) == -1){
-      mrb_raisef(mrb, E_RUNTIME_ERROR, "select failed: %s", mrb_str_new_cstr(mrb, strerror(errno)));
+      mrb_raisef(mrb, E_RUNTIME_ERROR, "select failed: %S", mrb_str_new_cstr(mrb, strerror(errno)));
       break;
     }
     

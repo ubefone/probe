8f517daf50e3accbee931e505a8391b2997a276a
Julien Ammous <schmurfy@gmail.com>
2016-10-21 18:06:29 +0200

    bumps version

--- main/tools/d3probe/d3probe.h
+++ main/tools/d3probe/d3probe.h
@@ -25,7 +25,7 @@
 #include "dmalloc.h"
 #endif
 
-#define PROBE_VERSION "1.7.1"
+#define PROBE_VERSION "1.7.2"
 
 #define C_CHECK(MSG, what) if(what == -1){ perror(MSG);  return -1; }
 
@@ -82,4 +82,3 @@ typedef struct {
   int       host_pipe;
   
 } Plugin;
-
